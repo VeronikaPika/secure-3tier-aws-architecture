191da81a2edce8db237d44821e1569eb0c51fb71
VeronikaPika <nikarelocates@outlook.com>
2026-05-18 00:20:14 +0100

    Test: Intentionally introducing bad resource block layout alignment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# Test Block to break the CI/CD layout engine
+resource     "aws_vpc"     "test_breaking_the_pipeline" {
+  cidr_block = "10.0.0.0/16"
+     tags = {
+  Name = "broken-vpc-alignment-test"
+    }
+}
